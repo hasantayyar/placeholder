a32e896c1cdba05be629a9a651b39d77942bb174
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2014-06-18 10:45:05 +0300

    Update index.php
    implemented custom text feature primitively

--- i/index.php
+++ i/index.php
@@ -7,11 +7,12 @@ $width           = isset($_GET['w']) ? trim($_GET['w']) : null;
 $height          = isset($_GET['h']) ? trim($_GET['h']) : null;
 $backgroundColor = isset($_GET['bgColor']) ? strtolower(trim($_GET['bgColor'])) : null;
 $textColor       = isset($_GET['textColor']) ? strtolower(trim($_GET['textColor'])) : null;
-
+$text 		 = isset($_GET['text']) ? strtoupper(trim($_GET['text'])) : null;
 try {
     $placeholder = new Placeholder();
     $placeholder->setWidth($width);
     $placeholder->setHeight($height);
+    $placeholder->setText($text);
     if ($backgroundColor) $placeholder->setBackgroundColor($backgroundColor);
     if ($textColor) $placeholder->setTextColor($textColor);
     $placeholder->render();
